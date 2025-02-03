e5a7de38e407188d2f2eec9c97d5dcdd9b7c01f5
borvux <bennyjoram@gmail.com>
2025-02-03 14:01:17 -0600

    created dices controller

---
+++ app/controllers/dices_controller.rb
@@ -0,0 +1,21 @@
+class DicesController < ApplicationController
+  def index
+
+  end
+
+  def two_six
+
+  end
+
+  def two_ten
+
+  end
+
+  def one_twenty
+
+  end
+
+  def five_four
+
+  end
+end
