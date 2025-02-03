1d40516c25ec497ad35529292b1dbf1bcda0145f
borvux <bennyjoram@gmail.com>
2025-02-03 13:58:26 -0600

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   # This is a blank app! Pick your first screen, build out the RCAV, and go from there. E.g.:
 
   # get "/your_first_screen" => "pages#first"
-  
+  get("/", {:controller => "dices", :action => "index"})
+  get("/dice/2/6", {:controller => "dices", :action => "two_six"})
+  get("/dice/2/10", {:controller => "dices", :action => "two_ten"})
+  get("/dice/1/20", {:controller => "dices", :action => "one_twenty"})
+  get("/dice/5/4", {:controller => "dices", :action => "five_four"})
 end
