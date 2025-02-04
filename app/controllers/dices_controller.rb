742122c0b2bbe244381c8fe2bc01a9753010b27c
borvux <bennyjoram@gmail.com>
2025-02-03 18:02:59 -0600

    added render templates on each action

--- app/controllers/dices_controller.rb
+++ app/controllers/dices_controller.rb
@@ -1,21 +1,21 @@
 class DicesController < ApplicationController
   def index
-
+    render({ :template => "dices/index" })
   end
 
   def two_six
-
+    render({ :template => "dices/two_six" })
   end
 
   def two_ten
-
+    render({ :template => "dices/two_ten" })
   end
 
   def one_twenty
-
+    render({ :template => "dices/one_twenty" })
   end
 
   def five_four
-
+    render({ :template => "dices/five_four" })
   end
 end
